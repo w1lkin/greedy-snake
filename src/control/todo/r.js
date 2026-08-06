2ad378ecb2e4635ac81e3d7a46df8eb353fcd80c
陈炎坤 <chenyankun@stocke.com.cn>
2026-08-06 08:19:30 +0800

    fix: 移除 lock 锁定，修复暂停/重开在 gameover 后卡死
    Bug 根因：overStart 设置 lock=true，导致 gameover 期间所有操作被锁
    
    修复：
    - overStart 不再设置 lock=true（贪吃蛇不需要 lock 机制）
    - todo.p/r.down 移除 lock 检查
    - todo.p 在 gameover 中调用 overEnd 终止动画后再 start

--- src/control/todo/r.js
+++ src/control/todo/r.js
@@ -3,12 +3,10 @@ import states from '../states'
 
 const downFn = store => {
   store.commit('key_reset', true)
-  if (store.state.lock) return
   event.down({
     key: 'r',
     once: true,
     callback: () => {
-      if (store.state.lock) return
       if (store.state.status === 'playing') {
         states.overStart(true)
       } else if (store.state.status === 'gameover') {
